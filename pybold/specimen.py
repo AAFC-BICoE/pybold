fed3c353f69d4d0e6a70259659195a8858ed495d
Iyad Kandalaft <ik@iyadk.com>
2015-11-22 01:25:15 -0500

    Implemented Specimen as a subclass of ObjectifiedElement
    There is still an issue with Specimen's attributes requiring the record
    prefix before accessing inner level attributes.

--- pybold/specimen.py
+++ pybold/specimen.py
@@ -3,36 +3,42 @@ Created on 2015-11-16
 
 @author: Iyad Kandalaft <iyad.kandalaft@agr.gc.ca
 '''
-from __builtin__ import super
-from booby import Model, fields
+from __builtin__ import super, str
+import __builtin__
 import json
-from lxml import objectify, etree
+from lxml import objectify
 import pickle
-from xml import sax
-from  xml.sax.handler import ContentHandler
+import xmltodict
 
 from pybold import PUBLIC_API_URL, Endpoint
 
 
-class Specimen(object):
+class Specimen(objectify.ObjectifiedElement):
     '''
     classdocs
     '''
-    record_id = fields.Integer()
-    processid = fields.String()
-
-    def __init__(self, obj):
-        '''
-        Constructor
-        '''
-        super(Specimen, self).__init__()
-        self = obj
     
+    def __new__(cls, objectified_element=None, specimen_record_xml=None):
+        if isinstance(specimen_record_xml, __builtin__.str):
+            return super(Specimen, cls).__new__(cls, specimen_record_xml = objectify.fromstring(specimen_record_xml))
+        elif isinstance(objectified_element, objectify.ObjectifiedElement):
+            return super(Specimen, cls).__new__(cls, objectified_element = objectified_element)
+    
+
+    def __init__(self, objectified_element=None, specimen_record_xml=None):
+        if isinstance(specimen_record_xml, __builtin__.str):
+            obj = objectify.fromstring(specimen_record_xml)
+        elif isinstance(objectified_element, objectify.ObjectifiedElement):
+            obj = objectified_element
+        
+        #print obj.getchildren()
+        super(Specimen, self).__init__(obj)
+
     def get_taxonomy(self):
-        print pickle.dumps(self)
-#          if self.taxonomy.phylum is not None:
-#              return self.taxonomy.phylum.taxon.name
-         
+        #if self.taxonomy.phylum is not None:
+         #   return self.taxonomy.phylum.taxon.name
+         print "taxonomy"
+     
 class Specimens(Endpoint):
     '''
     classDocstring
@@ -53,13 +59,17 @@ class Specimens(Endpoint):
                                     'geo': geo, 
                                     'format': 'xml'})
     
-        records = objectify.fromstring(result)
-        for record in records.record:
-            self.append(Specimen(record))
+        bold_specimens = objectify.fromstring(result)#.bold_records
+        #print bold_specimens.getchildren()
+        for specimen in bold_specimens.record:
+            self.append(Specimen(specimen))
         
 if __name__ == "__main__":
     test = Specimens()
     print test.url
     test.get(ids='ACRJP618-11|ACRJP619-11')
-    print test.pop().get_taxonomy()
-    print 'here'
+
+    #test.get(ids='ACRJP618-11')
+    print test.pop().getchildren()
+    #test.pop().get_taxonomy()
+    #print 'here'
