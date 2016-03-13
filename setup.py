e7ab58e59ea5de575577e40880438da3e6230270
Iyad Kandalaft <ik@iyadk.com>
2016-03-12 21:23:53 -0500

    Refs #6734 Fixed setup.py to support pushing to PyPi and install using pip. Removed requirements.txt.

--- setup.py
+++ setup.py
@@ -11,13 +11,16 @@
 from setuptools import setup
 
 setup(name='pybold',
-      version='0.1',
+      version='0.1.5',
       description='Barcode of Life Public API consumers in Python 2.7',
       author='Iyad Kandalaft',
       author_email='iyad.kandalaft@canada.ca',
+      maintainer='Iyad Kandalaft',
+      maintainer_email='iyad.kandalaft@canada.ca',
       license='LGPL v3',
-      url='https://pypi.python.org/pypi?name=pybold',
       packages=['pybold', 'tests'],
+      package_data={'': ['LICENSE']},
+      install_requires=['biopython', 'requests', 'lxml'],
       classifiers=[
                    "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
                    "Programming Language :: Python :: 2.7",
